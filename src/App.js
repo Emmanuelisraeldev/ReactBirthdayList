7e0bb99d942d804e08e6f383060d329057a2cba5
Emmanuel Israel <rexdmx@hotmail.com>
2022-04-05 22:25:55 +0400

    Created a small birthday list with a button which clears the list in order to practice the map method and state management

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+function App() {
+  const [people, setPeople] = useState(data)
+  return <main>
+        <section className='container'>
+          <h3>{people.length} birtdays today</h3>
+          {people.map((person)=>{
+            return(
+              <List key={person.id} {...person} />
+            )
+          })}        
+        <button onClick={()=>setPeople([])}>clear all</button>
+        </section>
+      </main>
+    
+}
+
+export default App;
